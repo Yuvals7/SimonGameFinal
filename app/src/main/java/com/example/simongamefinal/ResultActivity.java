08eb8077c0a9370151f84c1ff8981f562f4bd606
Yuvals7 <yuvi.shopen@gmail.com>
2024-05-31 10:48:00 +0300

    text to speech- German

--- app/src/main/java/com/example/simongamefinal/ResultActivity.java
+++ app/src/main/java/com/example/simongamefinal/ResultActivity.java
@@ -3,14 +3,37 @@ package com.example.simongamefinal;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.Voice;
 import android.util.Log;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
 
 public class ResultActivity extends AppCompatActivity {
 
     private TextView scoreTextView;
     private TextToSpeech tts;
+
+    private void convertTextToSpeech(String text) {
+        if (tts != null) {
+            Log.e("XXXXX", text );
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        }
+    }
+
+    private void setLanguageAndVoice() {
+        Locale desiredLocale = Locale.US; // Change to the desired language/locale
+        tts.setLanguage(desiredLocale);
+
+        Set<Voice> voices = tts.getVoices();
+        List<Voice> voiceList = new ArrayList<>(voices);
+        Voice selectedVoice = voiceList.get(10); // Change to the desired voice index
+        tts.setVoice(selectedVoice);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +52,24 @@ public class ResultActivity extends AppCompatActivity {
                 public void onInit(int status) {
                     if (status == TextToSpeech.SUCCESS) {
                         // TTS engine is successfully initialized.
-                        Log.e("XXXXX", "line 151 ");
+                        Log.e("XXXXX", "line 38 ");
+                        setLanguageAndVoice();
+                        convertTextToSpeech("Your score: " + score);
+
                     } else {
                         // Failed to initialize TTS engine.
-                        Log.e("XXXXX", "line 154 ");
+                        Log.e("XXXXX", "line 43 ");
                     }
                 }
             });
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
         }
     }
 }
